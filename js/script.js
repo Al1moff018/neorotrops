7648fbeb34a4cda0cf828fbd59ae23109dc10f5c
Al1moff018 <alimovmk018@gmail.com>
2025-06-25 20:52:10 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,40 @@
+    IMask(
+  document.getElementById('phone-mask'),
+  {
+    mask: '+{7}(000)000-00-00'
+  }
+)
+var swiper = new Swiper(".intro-swiper", {
+    navigation: {
+        nextEl: ".next-btn",
+        prevEl: ".prev-btn",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    scrollbar: {
+        el: ".swiper-scrollbar",
+      },
+});
+var swiper = new Swiper(".bottom-swiper", {
+    navigation: {
+        nextEl: ".next-btn-bottom",
+        prevEl: ".prev-btn-bottom",
+    },
+    pagination: {
+        el: ".swiper-pagination-bottom",
+        clickable: true,
+    },
+    scrollbar: {
+        el: ".swiper-scrollbar",
+      },
+});
+var swiper = new Swiper(".end-mini-swipers", {
+      slidesPerView: 3,
+      spaceBetween: 20,
+      pagination: {
+        el: ".swiper-pagination-end",
+        clickable: true,
+      },
+});
